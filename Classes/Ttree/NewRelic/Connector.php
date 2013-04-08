73820d4b326e17d5bc1167b769e0c2e3ad936638
Dominique Feyer <dfeyer@ttree.ch>
2013-04-08 20:19:37 +0200

    [TASK] Last Flow 2.0 change and cleanup

--- Classes/Ttree/NewRelic/Connector.php
+++ Classes/Ttree/NewRelic/Connector.php
@@ -17,17 +17,17 @@ class Connector {
     protected $settings;
 
     /**
+     * @var  \TYPO3\Flow\Log\SystemLoggerInterface $systemLogger
+     */
+    protected $systemLogger;
+
+    /**
      * @param array $settings
      * @return void
      */
     public function injectSettings(array $settings) {
         $this->settings = $settings;
     }
-
-    /**
-     * @var  \TYPO3\Flow\Log\SystemLoggerInterface $systemLogger
-     */
-    protected $systemLogger;
 
     /**
      * @param \TYPO3\Flow\Log\SystemLoggerInterface $systemLogger
@@ -37,92 +37,93 @@ class Connector {
         $this->systemLogger = $systemLogger;
     }
 
-	/**
-	 * @param \TYPO3\Flow\MVC\RequestInterface $request
-	 * @throws \Exception
-	 */
-	public function logRequest(\TYPO3\Flow\MVC\RequestInterface $request)  {
-        if(!extension_loaded('newrelic')) {
-            if($this->settings['logOnMissingExtension']) {
-                $this->systemLogger->log('newrelic extension missing - please install it');
+    /**
+     * @param \TYPO3\Flow\MVC\RequestInterface $request
+     * @throws \Exception
+     */
+    public function logRequest(\TYPO3\Flow\MVC\RequestInterface $request) {
+        if (!extension_loaded('newrelic')) {
+            if ($this->settings['logOnMissingExtension']) {
+                $this->systemLogger->log('newrelic extension missing - please install it', LOG_DEBUG, NULL, 'Ttree.NewRelic');
             }
-            if($this->settings['throwOnMissingExtension']) {
-                throw new \Exception('newrelic extension missing - install it');
+            if ($this->settings['throwOnMissingExtension']) {
+                throw new \Exception('newrelic extension missing - install it', 1365444902);
             }
         }
-        if($request instanceof \TYPO3\Flow\MVC\Web\Request ) {
-            return $this->logWebRequest($request);
+        if ($request instanceof \TYPO3\Flow\Mvc\ActionRequest) {
+            $this->logWebRequest($request);
+        } elseif ($request instanceof \TYPO3\Flow\Cli\Request) {
+            $this->logCliRequest($request);
+        } else {
+            $this->systemLogger->log('Request of unknown type');
         }
-        if($request instanceof \TYPO3\Flow\MVC\CLI\Request ) {
-            return $this->logCliRequest($request);
-        }
-        $this->systemLogger->log('Request of unknown type');
     }
 
-	/**
-	 * @param \TYPO3\Flow\MVC\Web\Request $request
-	 */
-	private function logWebRequest(\TYPO3\Flow\MVC\Web\Request $request) {
-        $values = array();
+    /**
+     * @param \TYPO3\Flow\Mvc\ActionRequest $request
+     */
+    private function logWebRequest(\TYPO3\Flow\Mvc\ActionRequest $request) {
+        $values                           = array();
         $values['{{FullControllerName}}'] = $request->getControllerObjectName();
-        $values['{{ControllerName}}'] = $request->getControllerName();
-        $values['{{Namespace}}'] = str_replace($values['{{ControllerName}}'], '', $request->getControllerObjectName());
-        $values['{{ActionName}}'] = $request->getControllerActionName();
-        $values['{{PackageKey}}'] = $request->getControllerPackageKey();
-        $values['{{UrlPath}}'] = $request->getRequestUri()->getPath();
-        $values['{{Format}}'] = $request->getFormat();
+        $values['{{ControllerName}}']     = $request->getControllerName();
+        $values['{{Namespace}}']          = str_replace($values['{{ControllerName}}'], '', $request->getControllerObjectName());
+        $values['{{ActionName}}']         = $request->getControllerActionName();
+        $values['{{PackageKey}}']         = $request->getControllerPackageKey();
+        $values['{{UrlPath}}']            = $request->getHttpRequest()->getUri()->getPath();
+        $values['{{Format}}']             = $request->getFormat();
 
-        $transactionName  =$this->formatTransactionName($this->getTransactionNameTemplate('Web'), $values);
+        $transactionName = $this->formatTransactionName($this->getTransactionNameTemplate('Web'), $values);
         $this->handleTransactionName($transactionName);
     }
 
-	/**
-	 * @param \TYPO3\Flow\MVC\CLI\Request $request
-	 */
-	private function logCliRequest(\TYPO3\Flow\MVC\CLI\Request $request) {
-        $values = array();
-        $values['{{FullControllerName}}'] = $request->getControllerObjectName();
-        $values['{{CommandName}}'] = $request->getControllerCommandName();
-        $values['{{ControllerName}}'] = \substr($request->getControllerObjectName(), \strrpos($request->getControllerObjectName(), '\\')+1 );
-        $values['{{Namespace}}'] = str_replace($values['{{ControllerName}}'], '', $request->getControllerObjectName());
-        $namespaceArray = explode('\\', $values['{{Namespace}}']);
-        $values['{{LastNameSpaceElementBeforeCommand}}'] = $namespaceArray[count($namespaceArray)-3];
-
-
-        $transactionName  = $this->formatTransactionName($this->getTransactionNameTemplate('Cli'), $values);
-
-        $this->handleTransactionName($transactionName);
-    }
-
-	/**
-	 * @param string $for
-	 * @return string
-	 */
-	private function getTransactionNameTemplate($for='Web') {
-        return $this->settings['transactionName']['template'][$for];
-    }
-
-	/**
-	 * @param string $transactionNameTemplate
-	 * @param array $values
-	 * @return mixed
-	 */
-	private function formatTransactionName($transactionNameTemplate, array $values) {
-        foreach($values as $key=>$value) {
+    /**
+     * @param string $transactionNameTemplate
+     * @param array $values
+     * @return mixed
+     */
+    private function formatTransactionName($transactionNameTemplate, array $values) {
+        foreach ($values as $key => $value) {
             $transactionNameTemplate = str_replace($key, $value, $transactionNameTemplate);
         }
+
         return $transactionNameTemplate;
     }
 
-	/**
-	 * @param string $transactionName
-	 */
-	private function handleTransactionName($transactionName) {
-		if ($this->settings['transactionName']['log']) {
-			$this->systemLogger->log($transactionName);
-		}
-		if ($this->settings['transactionName']['send'] && extension_loaded('newrelic')) {
-			newrelic_name_transaction($transactionName);
-		}
+    /**
+     * @param string $for
+     * @return string
+     */
+    private function getTransactionNameTemplate($for = 'Web') {
+        return $this->settings['transactionName']['template'][$for];
+    }
+
+    /**
+     * @param string $transactionName
+     */
+    private function handleTransactionName($transactionName) {
+        if ($this->settings['transactionName']['log']) {
+            $this->systemLogger->log($transactionName);
+        }
+        if ($this->settings['transactionName']['send'] && extension_loaded('newrelic')) {
+            newrelic_name_transaction($transactionName);
+        }
+    }
+
+    /**
+     * @param \TYPO3\Flow\Cli\Request $request
+     */
+    private function logCliRequest(\TYPO3\Flow\Cli\Request $request) {
+        $values                                          = array();
+        $values['{{FullControllerName}}']                = $request->getControllerObjectName();
+        $values['{{CommandName}}']                       = $request->getControllerCommandName();
+        $values['{{ControllerName}}']                    = \substr($request->getControllerObjectName(), \strrpos($request->getControllerObjectName(), '\\') + 1);
+        $values['{{Namespace}}']                         = str_replace($values['{{ControllerName}}'], '', $request->getControllerObjectName());
+        $namespaceArray                                  = explode('\\', $values['{{Namespace}}']);
+        $values['{{LastNameSpaceElementBeforeCommand}}'] = $namespaceArray[count($namespaceArray) - 3];
+
+
+        $transactionName = $this->formatTransactionName($this->getTransactionNameTemplate('Cli'), $values);
+
+        $this->handleTransactionName($transactionName);
     }
 }
